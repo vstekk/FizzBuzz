0c9c68ca86313c75c8ea07dc3f8dfaddc3f2652f
vstekk <jakub.racek@gmail.com>
2024-01-22 09:02:12 +0100

    enterprised fizzbuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,35 +1,31 @@
-﻿// vypisovat cisla postupne
-// cisla delitelne 3 napisu misto toho Fizz
-// cisla delitelna 5 napisu misto toho Buzz
-// cisla delitelna 3 i 5 jsou FizzBuzz
-
-
-int limit;
+﻿int limit;
 string input;
 
-Console.WriteLine("Give me your FizzBuzz limit:");
 do
 {
+    Console.Clear();
+    Console.WriteLine("Write a number and press [Enter]");
     input = Console.ReadLine() ?? string.Empty;
 } while (!int.TryParse(input, out limit));
 
-Console.Clear();
 
 for (var i = 1; i <= limit; i++)
 {
-    Console.WriteLine(GetFizzBuzzString(i));
+    Console.WriteLine(AsFizzBuzz(i));
 }
 
-string GetFizzBuzzString(int number)
+string AsFizzBuzz(int number)
 {
-    return
-        IsDivisibleBy(number, 3) && IsDivisibleBy(number, 5) ? "FizzBuzz"
-        : IsDivisibleBy(number, 3) ? "Fizz"
-        : IsDivisibleBy(number, 5) ? "Buzz"
-        : number.ToString();
+    return (IsDivisibleBy(number, 3), IsDivisibleBy(number, 5)) switch
+    {
+        (true, true) => "FizzBuzz",
+        (true, false) => "Fizz",
+        (false, true) => "Buzz",
+        _ => number.ToString()
+    };
 }
 
-bool IsDivisibleBy(int number, int divider)
+bool IsDivisibleBy(int number, int divisor)
 {
-    return number % divider == 0;
+    return number % divisor == 0;
 }
